b6d2394ca4b377b5bd30d06a227caed9573c672c
Ismailodabashi <43229782+Ismailodabashi@users.noreply.github.com>
2019-03-28 09:28:39 +0300

    Update All.lisp

--- All.lisp
+++ All.lisp
@@ -41,11 +41,15 @@
 ;Определите функцию, удаляющие в исходном списке все повторные вхождения элементов.
 
 (defun double (lst &optional (d nil))
-      ((lambda (x) (and(setq first (car x))(setq last (cdr x)))) lst)
+      ((lambda (first last)
             (cond ((null first) (list d))
                   ((eq (member first last) nil) (double last (cons first d)))
                   (t (double last d))
             )
+               )
+               (car lst) (cdr lst)
+               )
+	
 )
 
 ;(double '(9 1 3 2 1 2 2 2 3 2 3 4 1 5 4 6 3))
@@ -75,10 +79,14 @@
 
 
 (defun udal (lst a)
-    ((lambda (x) (and(setq first (car x))(setq last (cdr x)))) lst)
-        (cond ((null first) lst)
-              ((= first a) last)
-              (t (cons first (udal (cdr lst) a))))
+    ((lambda (first last) 
+              (cond ((null first) lst)
+                     ((= first a) last)
+                     (t (cons first (udal last a)))
+              )
+						  
+		) (car lst) (cdr lst))
+        
 
 )
 
@@ -92,11 +100,13 @@
 ;Определите функцию, удаляющую из списка каждый четный элемент.
 
 (defun chet (lst &optional (n nil))
-    ((lambda (x) (and(setq first (car x))(setq last (cdr x)))) lst)
-        (cond ((null first) (list n))
-              ((/= (rem first 2) 0) (chet last (cons first n)))
-              (t (chet last n))
+    ((lambda (first last) 
+              (cond ((null first) (list n))
+                    ((/= (rem first 2) 0) (chet last (cons first n)))
+                    (t (chet last n))
         )
+             
+             ) (car lst)(cdr lst))
  )
  
  ;(chet '(9 1 3 2 1 2 2 2 3 2 3 4 1 5 4 6 3))
@@ -107,12 +117,14 @@
  ;Задача 28
  ;Определите функцию, вычисляющую, сколько всего атомов в списке.
  
- (defun atm (lst &optional (kol 0))
-    ((lambda (x) (and(setq first (car x))(setq last (cdr x)))) lst)
-      (cond ((null lst) kol)
-            ((atom first) (atm last (+ kol 1)))
-            (t (atm last kol))
-      )
+(defun atm (lst &optional (kol 0))
+    ((lambda (first last) 
+             (cond ((null lst) kol)
+                   ((atom first) (atm last (+ kol 1)))
+                   (t (atm last kol))
+             )
+      ) (car lst) (cdr lst))
+      
 )
 
 ;(atm '(1 2 3 4 (5 6)))
@@ -125,11 +137,13 @@
 
 
 (defun mnoj (lst)
-	((lambda (x) (and(setq first (car x))(setq last (cdr x)))) lst)
-		(cond ((null lst) 'True)
-			  ((member first last) 'False)
-			  (t (mnoj last))
-		)
+	((lambda (first last) 
+              (cond ((null lst) 'True)
+			        ((member first last) 'False)
+			        (t (mnoj last))
+		      )
+     ) (car lst)(cdr lst))
+		
 )
 
 ;(mnoj '(1 2 2 3 4 5))
@@ -140,33 +154,40 @@
 ;Задача 45
 ;Напишите функцию (РАССТОЯНИЕ a b), вычисляющую расстояние между городами a b.
 
-(setf (get 'Simf 'x) '-10)
-(setf (get 'Simf 'y) '2)
-(setf (get 'Bahc 'x) '20)
-(setf (get 'Bahc 'y) '-25)
-(setf (get 'Sev 'x) '24)
-(setf (get 'Sev 'y) '-31)
+(defun prop (city1 city2 xy1 xy2)
+	(setf (get city1 'x) (car xy1))
+	(setf (get city1 'y) (cadr xy1))
+	(setf (get city2 'x) (car xy2))
+	(setf (get city2 'y) (cadr xy2))
+	(dist 'city1 'city2)
+)
+
 
 (defun dist (a b)
     ((lambda (x1 y1 x2 y2) (sqrt(+ (* (- x2 x1) (- x2 x1)) (* (- y2 y1) (- y2 y1))))) (get a 'x)(get a 'y)(get b 'x)(get b 'y))
 )
 
-;(dist 'Bahc 'Sev)
-;7.2111025
-;(dist 'Simf 'Bahc)
-;40.36087 
+
+;(prop 'city1 'city2 '(-10 2) '(20 -25))
+;40.36087
+;(dist 'city1 'city2 '(2 3) '(3 8))
+;5.0990195 
 
 ;Задача 46
 ;Напишите функцию (РОДИТЕЛИ x) и (СЕСТРЫ_БРАТЬЯ x1 x2).
 
-(setf (get 'Ismail 'dad) 'Ernes)
-(setf (get 'Ismail 'mom) 'Elzara)
-(setf (get 'Enver 'dad) 'Ernes)
-(setf (get 'Enver 'mom) 'Elzara)
-(setf (get 'Ernes 'dad) 'Bekir)
-(setf (get 'Ernes 'mom) 'Suvade)
-(setf (get 'Elvina 'mom) 'Zarema)
-(setf (get 'Elvina 'dad) 'Ernes)
+(defun prop (name dadmom)
+    (setf (get name 'dad) (car dadmom))
+    (setf (get name 'mom) (cadr dadmom))
+)
+ 
+(defun parsisbro (name1 name2 dadmom1 dadmom2)
+    (prop name1 dadmom1)
+    (prop name2 dadmom2)
+    (print (parents name1))
+    (print (parents name2))
+    (sis-bro name1 name2)
+)
 
 (defun parents (x)
     (cons (get x 'dad) (get x 'mom))
@@ -176,12 +197,15 @@
     (if (or (eq (get x1 'dad) (get x2 'dad)) (eq (get x1 'mom) (get x2 'mom))) t nil)
 )
 
-;(parents 'Enver)
-;(ERNES . ELZARA) 
-;(sis-bro 'Enver 'Elvina
-;T
-;(sis-bro 'Enver 'Suvade) 
+;(parsisbro 'Enver 'Ismail '(Bekir Elzara) '(Ernes Suvade))
+;(BEKIR . ELZARA) 
+;(ERNES . SUVADE) 
 ;NIL
+
+;(parsisbro 'Ernes 'Sadive '(Bekir Suvade) '(Bekir Suvade))
+;(BEKIR . SUVADE) 
+;(BEKIR . SUVADE) 
+;T 
 
 
 
