24c50cbe86a37514f24c9b0164a7a0ed85a9f755
Ismailodabashi <43229782+Ismailodabashi@users.noreply.github.com>
2019-05-15 00:41:42 +0300

    Delete 8.lisp

--- 8.lisp
+++
@@ -1,9 +0,0 @@
-(defun task (lst &optional (p nil) (n nil))
-  (cond ((null (car lst)) (list p n))
-        ((> (car lst) 0) (task (cdr lst) (cons (car lst) p) n))
-        ((< (car lst) 0) (task (cdr lst) p (cons (car lst) n)))
-        ((= (car lst) 0) (task (cdr lst) p n))
-  )
-) 
-
-(task '(1 2 3 4 0 -2 3 -3))
