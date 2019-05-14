d88afeb75072755c82cbd864751d4afacffaf638
Ismailodabashi <43229782+Ismailodabashi@users.noreply.github.com>
2019-05-15 00:41:26 +0300

    Delete 25.lisp

--- 25.lisp
+++
@@ -1,9 +0,0 @@
-(defun chet (lst &optional (n nil))
-    (cond ((null (car lst)) (list n))
-          ((/= (rem (car lst) 2) 0) (chet (cdr lst) (cons (car lst) n)))
-          (t (chet (cdr lst) n))
-    )
- )
-
-(car (chet '(9 1 3 2 1 2 2 2 3 2 3 4 1 5 4 6 3))))
-
